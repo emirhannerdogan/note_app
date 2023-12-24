c667ca1c119351bede13b4e46caf991230f5c4c5
YasinSefa15 <yasekirman@gmail.com>
2023-12-25 00:38:32 +0300

    permission etc

--- lib/bluetooth_handler.dart
+++ lib/bluetooth_handler.dart
@@ -8,24 +8,30 @@ class BluetoothHandler extends StatefulWidget {
   @override
   _BluetoothHandlerState createState() => _BluetoothHandlerState();
 }
+
 class _BluetoothHandlerState extends State<BluetoothHandler> {
   FlutterBlue ble = FlutterBlue.instance;
   List<BluetoothDevice> foundDevices = [];
-  
-  void startBluetoothProcess(BuildContext context) {
-    foundDevices.clear(); // Temizleme işlemi her başlatıldığında yapılsın.
-    
-    
-  }
+
+  void startBluetoothProcess() async {}
 
   Future scanDevices() async {
-    ble.startScan(timeout: const Duration(seconds: 4));
+    var status = await Permission.bluetoothScan.status;
+    var status1 = await Permission.bluetoothConnect.status;
+    debugPrint(status.toString());
+    debugPrint(status1.toString());
+
+    debugPrint("Taranıyor");
+    foundDevices.clear(); // Temizleme işlemi her başlatıldığında yapılsın.
+    ble.startScan(timeout: const Duration(seconds: 4)).then((value) {
+      debugPrint("Tarama süresi doldu...");
+    });
 
     ble.scanResults.listen((List<ScanResult> results) {
       for (ScanResult r in results) {
         setState(() {
-          if(r.device.name != '' && !foundDevices.contains(r.device)) {
-             foundDevices.add(r.device);
+          if (r.device.name != '' && !foundDevices.contains(r.device)) {
+            foundDevices.add(r.device);
           }
         });
         debugPrint('${r.device.name} found! rssi: ${r.rssi}');
@@ -33,47 +39,46 @@ class _BluetoothHandlerState extends State<BluetoothHandler> {
         debugPrint(r.advertisementData.toString());
       }
     });
-
   }
+
   @override
   void initState() {
-    startBluetoothProcess(context);
+    //startBluetoothProcess();
     super.initState();
   }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Scaffold(
-        appBar: AppBar(
-          title: const Text("Bluetooth"),
-        ),
-        body: Column(
-          children: [
-            ElevatedButton(
-              onPressed: () {
-                scanDevices();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Bluetooth"),
+      ),
+      body: Column(
+        children: [
+          ElevatedButton(
+            onPressed: () {
+              scanDevices();
+            },
+            child: const Text("Cihazları Tara"),
+          ),
+          Expanded(
+            child: ListView.builder(
+              itemCount: foundDevices.length,
+              itemBuilder: (context, index) {
+                return ListTile(
+                  title: Text(foundDevices[index].name),
+                  subtitle: Text(foundDevices[index].id.toString()),
+                  trailing: ElevatedButton(
+                    onPressed: () {
+                      // Cihazı eşleştir
+                    },
+                    child: const Text("Eşleştir"),
+                  ),
+                );
               },
-              child: const Text("Cihazları Tara"),
             ),
-            Expanded(
-              child: ListView.builder(
-                itemCount: foundDevices.length,
-                itemBuilder: (context, index) {
-                  return ListTile(
-                    title: Text(foundDevices[index].name),
-                    subtitle: Text(foundDevices[index].id.toString()),
-                    trailing: ElevatedButton(
-                      onPressed: () {
-                        // Cihazı eşleştir
-                      },
-                      child: const Text("Eşleştir"),
-                    ),
-                  );
-                },
-              ),
-            ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
